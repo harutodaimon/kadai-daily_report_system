f74f91b2e0d3e56ac0f50375d9da92f8cd20deb3
haruto.daimon <h.daimon@bondstech.jp>
2024-06-12 18:17:17 +0900

    lesson8完了

--- Sample04/src/test/RPG.java
+++ Sample04/src/test/RPG.java
@@ -1,15 +1,20 @@
 package test;
 
+import l06.c03.s01.Enemy;
+import l06.c03.s01.Hero;
+
 public class RPG {
 
 	public static void main(String[] args) {
 
-		Character hero = new Character("勇者", 10, 5, 3);
-		Character slime = new Character("スライム", 5, 2, 2);
+		Hero hero = new Hero("勇者", 10, 5, 3, 1);
+		Enemy slime = new Enemy("スライム", 5, 2, 2, "スライム");
 
-		hero.attack(slime);
-		slime.attack(hero);
+		//hero.attack(slime);
+		//slime.attack(hero);
 
+
+		hero.magic_attack(slime);
 	}
 
 }
